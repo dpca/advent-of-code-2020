c0c63c32d6497c815dda4d6a27989b977c35fd23
Dan Cahoon <dpcahoon@gmail.com>
2020-12-15 15:25:26 -0500

    Day 15

---
+++ day15/day15.hs
@@ -0,0 +1,17 @@
+import Data.Map
+
+numberAt :: [Int] -> Int -> Int
+numberAt startingNumbers end = go 1 (head startingNumbers) empty
+    where startLen = length startingNumbers
+          go idx prevNum prevMap
+            | idx == end             = prevNum
+            | idx < startLen         = go nextIdx (startingNumbers !! idx) newMap
+            | member prevNum prevMap = go nextIdx (idx - (prevMap ! prevNum)) newMap
+            | otherwise              = go nextIdx 0 newMap
+            where nextIdx = idx + 1
+                  newMap = insert prevNum idx prevMap
+
+main = do
+    let input = [1,0,16,5,17,4]
+    print $ "Part 1: " ++ show (numberAt input 2020)
+    print $ "Part 2: " ++ show (numberAt input 30000000)
